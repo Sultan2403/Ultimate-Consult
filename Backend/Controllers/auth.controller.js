28b13d0dca24840fc3ab3b8b48c11af9ea026cce
Sultan2403 <sultanremilekun@gmail.com>
2026-02-24 14:11:32 +0100

    init auth controller

---
+++ Backend/Controllers/auth.controller.js
@@ -0,0 +1,41 @@
+const adminCollection = require("../DB/Models/admin.model");
+const bcrypt = require("bcryptjs");
+const { generateNewTokens } = require("../Utils/tokens.util");
+
+const loginAdmin = async (req, res) => {
+  try {
+    const { password, email } = req.body;
+    const existingAccount = await adminCollection
+      .findOne({ email })
+      .select("+password");
+
+    if (!existingAccount) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    const passwordsMatch = await bcrypt.compare(
+      password,
+      existingAccount.password,
+    );
+
+    if (!passwordsMatch) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    const user = existingAccount.toJSON();
+    const tokens = await generateNewTokens(user);
+
+    res.status(200).json({ success: true, tokens });
+  } catch (error) {
+    console.error(error, error.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "An error occurred" });
+  }
+};
+
+module.exports = { loginAdmin };
