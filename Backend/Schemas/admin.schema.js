f0dab6b23d6155b9ab0c5b4af28752464b6c82bc
Sultan2403 <sultanremilekun@gmail.com>
2026-02-24 14:09:15 +0100

    admin schema done

--- Backend/Schemas/admin.schema.js
+++ Backend/Schemas/admin.schema.js
@@ -1,2 +1,10 @@
-const {Joi} = require("celebrate")
+const { Joi } = require("celebrate");
 
+const adminLoginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+})
+  .required()
+  .options({ stripUnknown: true });
+
+module.exports = { adminLoginSchema };
