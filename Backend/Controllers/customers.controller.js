70bc01fec5aef8b1c27067b3eb10fc5da685002b
Sultan2403 <sultanremilekun@gmail.com>
2026-02-28 22:32:27 +0100

    customer creation logic up and running. Awaiting a refactor

--- Backend/Controllers/customers.controller.js
+++ Backend/Controllers/customers.controller.js
@@ -1,18 +1,40 @@
 const customersCollection = require("../DB/Models/customers.model");
+const transactionsCollection = require("../DB/Models/transactions.model");
 
 const addNewCustomer = async (req, res) => {
   const consultationStatus = "Pending";
+  const accessToken = req.params.token;
   const data = { ...req.body, consultationStatus };
 
+
   try {
+    // Try and find and update transaction in db
+
+    const updatedTransaction = await transactionsCollection.findOneAndUpdate(
+      { accessToken, status: "Processing" },
+      { tokenUsed: true },
+      { new: true },
+    );
+
+    if (!updatedTransaction) {
+      return res.status(404).json({
+        success: false,
+        message: "Transaction not found or token already used",
+      });
+    }
+
     await customersCollection.create(data);
     res
       .status(201)
-      .json({ success: true, message: "Customer added successfully" });
+      .json({
+        success: true,
+        message:
+          "Your request was successful. We'll get in touch with you within 24 hours",
+      });
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: "An error occured while adding the customer",
+      message: "An error occured while processing your request",
       error: error.message,
     });
   }
