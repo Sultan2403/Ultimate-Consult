d1f37ab718ddeef66c5f02d7d3162628dfc0d222
Sultan2403 <sultanremilekun@gmail.com>
2026-03-01 08:35:06 +0100

    update to customer router for admin view for customers

--- Backend/Routers/customer.routes.js
+++ Backend/Routers/customer.routes.js
@@ -2,9 +2,11 @@ const { celebrate } = require("celebrate");
 const express = require("express");
 const { addNewCustomer } = require("../Controllers/customers.controller");
 const { customerSchema } = require("../Schemas/customers.schema");
+const authMiddleware = require("../Middlewares/admin.auth");
 
 const router = express.Router();
 
 router.post("/", celebrate({ body: customerSchema }), addNewCustomer)
+router.get("/", authMiddleware, addNewCustomer)
 
 module.exports = router;
