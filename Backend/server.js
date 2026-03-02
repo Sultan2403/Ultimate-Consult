ebf3e7a807d023a3985c1ad2723332f195acb12f
Sultan2403 <sultanremilekun@gmail.com>
2026-03-02 03:11:05 +0100

    removed unused import and fixed up formatting in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
-const { initSocket, getIO } = require("./Utils/sockets");
+const { initSocket } = require("./Utils/sockets");
 
 const app = require("./app");
 const http = require("http");
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
-initSocket(server);
 
+initSocket(server);
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
