615880e4b1a67989f1b31e8e5d20993a00a20885
Sultan2403 <sultanremilekun@gmail.com>
2026-03-02 03:37:35 +0100

    payment service webhook integrated w/ websocket for real time updates for client

--- Backend/Services/payments.service.js
+++ Backend/Services/payments.service.js
@@ -1,13 +1,21 @@
 const transactionsCollection = require("../DB/Models/transactions.model");
+const { getIO } = require("../Utils/sockets.js");
 
 const handleChargeSuccess = async (reference) => {
   try {
     // Update transaction in db
-    await transactionsCollection.findOneAndUpdate(
+    const transaction = await transactionsCollection.findOneAndUpdate(
       { reference, status: "Processing" },
       { status: "Successful" },
       { new: true },
     );
+
+    if (transaction) {
+      const io = getIO();
+      io.to(transaction.accessToken).emit("payment.success");
+    } else {
+      throw new Error("Failed to update transaction");
+    }
   } catch (error) {
     throw new Error("Error processing successful charge: " + error.message);
   }
@@ -16,11 +24,18 @@ const handleChargeSuccess = async (reference) => {
 const handleChargeFailure = async (reference) => {
   try {
     // Marks transaction as failed in db
-    await transactionsCollection.findOneAndUpdate(
+    const transaction = await transactionsCollection.findOneAndUpdate(
       { reference, status: "Processing" },
       { status: "Failed" },
       { new: true },
     );
+
+    if (transaction) {
+      const io = getIO();
+      io.to(transaction.accessToken).emit("payment.failure");
+    } else {
+      throw new Error("Failed to update transaction");
+    }
   } catch (error) {
     throw new Error("Error processing failed charge: " + error.message);
   }
