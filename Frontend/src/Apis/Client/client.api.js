46240206ce8e1d7ec950a74029a4084b800824ad
Sultan2403 <sultanremilekun@gmail.com>
2026-04-24 16:47:45 +0100

    delete unused stuff + defensive programming

--- Frontend/src/Apis/Client/client.api.js
+++ Frontend/src/Apis/Client/client.api.js
@@ -4,7 +4,7 @@ const url = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: url,
-  timeout: url.includes("localhost") ? 10000 : 60000,
+  timeout: url?.includes("localhost") ? 10000 : 60000,
 });
 
 api.interceptors.response.use((res) => res.data);
