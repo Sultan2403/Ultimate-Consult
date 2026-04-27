9fb229fb53d9e47b41a4387052c5512bc7daebe6
Sultan2403 <sultanremilekun@gmail.com>
2026-04-27 14:21:35 +0100

    moved token header parsing to a try catch so server doesnt crash in case of malformed header

--- Backend/Middlewares/auth.js
+++ Backend/Middlewares/auth.js
@@ -3,17 +3,20 @@ const { verifyAccessToken } = require("../Utils/Auth/tokens.util");
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).json({ success: false, message: "No token provided" });
+    return res
+      .status(401)
+      .json({ success: false, message: "No token provided" });
   }
 
-  const token = authHeader.split(" ")[1];
-
   try {
+    const token = authHeader.split(" ")[1];
     const decoded = verifyAccessToken(token);
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(401).json({  success: false, message: "Invalid or expired token" });
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid or expired token" });
   }
 };
 
