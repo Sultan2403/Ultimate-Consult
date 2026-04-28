f08cdd432de6257d65a080a8fda6710c4ce77b92
Sultan2403 <sultanremilekun@gmail.com>
2026-04-28 02:38:44 +0100

    added redis config log for debugging of prod

--- Backend/Config/constants.js
+++ Backend/Config/constants.js
@@ -1,17 +1,19 @@
 const REDIS_CONFIG = {
-    HOST: process.env.REDIS_HOST || "localhost",
-    PORT: process.env.REDIS_PORT || 6379
-}
+  HOST: process.env.REDIS_HOST || "localhost",
+  PORT: process.env.REDIS_PORT || 6379,
+};
+
+console.log(REDIS_CONFIG.HOST);
 
 const QUEUES = {
-  NOTIFICATIONS: 'notifications_queue',
+  NOTIFICATIONS: "notifications_queue",
 };
 
 const JOBS = {
   NOTIFICATIONS: {
     // Admin specific
-    ADMIN_NEW_CONSULT: 'job_admin_new_consultation',
-  }
+    ADMIN_NEW_CONSULT: "job_admin_new_consultation",
+  },
 };
 
-module.exports = { REDIS_CONFIG, QUEUES, JOBS }
+module.exports = { REDIS_CONFIG, QUEUES, JOBS };
